6a2836747e12c0a63629f495a0ee5a4daf5918b8
Reece Morgan <reece.morgan@footballpools.com>
2023-06-29 10:40:17 +0100

    removed redux from index

--- apps/nextjs-training/pages/index.tsx
+++ apps/nextjs-training/pages/index.tsx
@@ -1,21 +1,7 @@
-import { selectAuthState, setAuthState } from "../store/authSlice";
-import { useDispatch, useSelector } from "react-redux";
-
 const Home = () => {
-  const authState = useSelector(selectAuthState);
-  const dispatch = useDispatch();
   return (
     <div>
-      <div>{authState ? "Logged in" : "Not Logged In"}</div>
-      <button
-        onClick={() =>
-          authState
-            ? dispatch(setAuthState(false))
-            : dispatch(setAuthState(true))
-        }
-      >
-        {authState ? "Log Out" : "Log In"}
-      </button>
+      <p>Site Content</p>
     </div>
   );
 };
